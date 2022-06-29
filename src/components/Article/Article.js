6a9d4a23b77985bacedb8c10046dcefaadf92ef3
Kacper Żarna <kacper-zarna@wp.pl>
2022-06-29 21:35:11 +0200

    Dodanie article.js

---
+++ src/components/Article/Article.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Article = () => {
+  return (
+    <li>
+      <a href="#">
+        <img src="/" alt="alt" />
+        <span>123 Span 123</span>
+      </a>
+    </li>
+  );
+};
+
+export default Article;
