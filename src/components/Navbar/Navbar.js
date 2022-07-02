3301ccbded1d317f83aa257f84bec77bd5b74300
Kacper Żarna <kacper-zarna@wp.pl>
2022-07-02 11:54:04 +0200

    Kod 02.07.2022 12:00

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,21 @@
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+
 import { Link } from "react-router-dom";
 
-const Navbar = (props) => {
-  return (
-    <nav>
-      <h1>SDA NEWS</h1>
-      <Link to="/">
+{
+  /* <Link to="/">
         <button>Home</button>
       </Link>
       <Link to="/search">
@@ -13,8 +23,146 @@ const Navbar = (props) => {
       </Link>
       <Link to="/login">
         <button>Login</button>
-      </Link>
-    </nav>
+      </Link> */
+}
+
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const Navbar = (props) => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "Roboto",
+              fontWeight: 100,
+              letterSpacing: ".1rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            SDA NEWS
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem>
+                <Link to="/" style={{ textDecoration: "none" }}>
+                  <Typography textAlign="center" onClick={handleCloseNavMenu}>
+                    Home
+                  </Typography>
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <Link to="/search" style={{ textDecoration: "none" }}>
+                  <Typography textAlign="center" onClick={handleCloseNavMenu}>
+                    Search
+                  </Typography>
+                </Link>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "Monospace",
+              fontWeight: 100,
+              letterSpacing: ".1rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            SDA NEWS
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Home
+              </Button>
+            </Link>
+            <Link to="/search" style={{ textDecoration: "none" }}>
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Search
+              </Button>
+            </Link>
+            {/* )) */}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Link to="login" style={{ textDecoration: "none" }}>
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/" />
+              </IconButton>
+            </Link>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 
